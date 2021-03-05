65c27624a52e2a30c1d9d5642adc114cb59ccf3d
redbudi <red.budi@yahoo.com>
2021-03-05 21:36:19 +0700

    memperbaiki home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 public function index(){
-// $data=array('web' => $this->Model_app->getSelectedData('t_perusahaan','id','1')->row(),);
-$this->load->view('home');
+$data=array('title' =>"PET ONLINE");
+$this->load->view('home',$data);
 } 
 }
