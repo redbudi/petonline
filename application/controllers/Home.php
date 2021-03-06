75a830f12282a0d1ab11b8bde0516ca6ad2c6e24
redbudi <red.budi@yahoo.com>
2021-03-06 13:32:40 +0700

    perbaikan tampilan web

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,7 +1,34 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
+
 public function index(){
 $data=array('title' =>"PET ONLINE");
+$this->load->view('header');
 $this->load->view('home',$data);
+} 
+
+public function contact(){
+$data=array('title' =>"PET ONLINE");
+$this->load->view('header');
+$this->load->view('contact',$data);
+} 
+
+public function service(){
+$data=array('title' =>"PET ONLINE");
+$this->load->view('header');
+$this->load->view('service',$data);
+} 
+
+
+public function login(){
+$data=array('title' =>"PET ONLINE");
+$this->load->view('header');
+$this->load->view('login',$data);
+} 
+
+public function registration(){
+$data=array('title' =>"PET ONLINE");
+$this->load->view('header');
+$this->load->view('registration',$data);
 } 
 }
